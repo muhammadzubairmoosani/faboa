f879e0bb206b1a3cbf23bdf86785ce9ac69b0cb2
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2024-06-06 15:48:09 +0500

    complete member screen

--- app/components/status.tsx
+++ app/components/status.tsx
@@ -6,8 +6,7 @@ interface StatusProps {
 
 export const Status = ({ status }: StatusProps) => (
   <div
-    className={`flex items-center space-x-2  py-1 px-3 rounded-lg pr-5 border-2`}
-    style={{ borderColor: "#BFC2BD" }}
+    className={`flex items-center space-x-2  py-1 px-3 rounded-lg pr-5 border-2 border-[#BFC2BD]`}
   >
     {status === "Approved" ? (
       <Image src="/active.svg" alt="active" width={20} height={20} />
